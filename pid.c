686bda5e3a7b9a20ee80855dfae98965d187ccbe
Luiz Felipe Fonseca Rosa <luizfelipefonsecarosa@gmail.com>
2023-08-30 08:49:14 -0300

    pid

---
+++ pid.c
@@ -0,0 +1,11 @@
+#include <sys/syscall.h>
+#include <stdio.h>
+
+int main(){
+    long mypid, ppid;
+    mypid = syscall(SYS_getpid);
+    ppid = syscall(SYS_getppid);
+
+    printf("My PID: %ld\n", mypid);
+    printf("Parent PID: %ld\n", ppid);
+}
